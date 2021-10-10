e4db20082aceda64b082e0a9a6fe2a344d84202c
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2021-10-10 15:32:03 +0200

    improve test execution reduce voyage_cover size

--- app/src/test/kotlin/dev/shaarawy/voyage/data/repositories/ArticlesRepositoryImplTest.kt
+++ app/src/test/kotlin/dev/shaarawy/voyage/data/repositories/ArticlesRepositoryImplTest.kt
@@ -1,59 +1,45 @@
 package dev.shaarawy.voyage.data.repositories
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import dev.shaarawy.voyage.data.services.ArticlesService
+import com.google.common.truth.Truth.assertThat
+import dev.shaarawy.voyage.readTextFile
+import dev.shaarawy.voyage.rules.MainCoroutineRule
+import dev.shaarawy.voyage.rules.RetrofitRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import org.junit.Test
-import retrofit2.Retrofit
-import com.google.common.truth.Truth.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import org.junit.After
-import org.junit.Before
-import java.io.File
+import org.junit.Rule
+import org.junit.Test
 
 
 /**
  * @author Mohamed Elshaarawy on Oct 10, 2021.
  */
 @ExperimentalSerializationApi
+@ExperimentalCoroutinesApi
 class ArticlesRepositoryImplTest {
 
-    private val server by lazy { MockWebServer() }
+    @get:Rule
+    val retrofitRule by lazy { RetrofitRule() }
 
-    private val articlesService by lazy {
-        Retrofit.Builder()
-            .baseUrl(server.url("/"))
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-            .build().create(ArticlesService::class.java)
-    }
+    @get:Rule
+    val coroutineRule by lazy { MainCoroutineRule() }
 
-    private val repo by lazy { ArticlesRepositoryImpl(articlesService) }
+    private val server by lazy { retrofitRule.server }
 
-    @Before
-    fun setUp() {
-        server.start()
-    }
-
-    @After
-    fun tearDown() {
-        server.shutdown()
-    }
+    private val repo by lazy { ArticlesRepositoryImpl(retrofitRule.createService()) }
 
     @Test
     fun `retrieve data`() = runBlocking {
         val r = MockResponse()
-        r.setBody(readJSONFile("articles.json"))
+        r.setBody(readTextFile("articles.json"))
         server.enqueue(r)
         assertThat(repo.getArticles(5)).hasSize(5)
     }
 
     @Test
     fun `not found id`() = runBlocking {
-        val r200 = MockResponse().apply { setBody(readJSONFile("article.json")) }
+        val r200 = MockResponse().apply { setBody(readTextFile("article.json")) }
         val r404 = MockResponse().apply { setResponseCode(404) }
         val r500 = MockResponse().apply { setResponseCode(500) }
         server.enqueue(r200)
@@ -64,11 +50,4 @@ class ArticlesRepositoryImplTest {
         val result = runCatching { repo.getArticleById(-1) }
         assertThat(result.exceptionOrNull()).isNotNull()
     }
-
-    private fun readJSONFile(name: String): String {
-        val classLoader = javaClass.classLoader!!
-        return File(classLoader.getResource(name).file).readText()
-    }
-
-
 }
